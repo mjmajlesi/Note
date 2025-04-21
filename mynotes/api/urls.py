357692c63e2e1b7c2f073e41cf4257f886ed8035
محمد جواد مجلسی <mohammadjavadmajlesi313@gmail.com>
2025-04-21 15:42:04 +0330

    add python-cors for cors bug in fetch api

--- mynotes/api/urls.py
+++ mynotes/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getNotes , name="notes"),
-    path('notes/<str:pk>/' , views.getNote , name="note")
+    path('api/notes', views.getNotes , name="notes"),
+    path('api/notes/<str:pk>/' , views.getNote , name="note")
 ]
